1a78f6a9b988111ec1ed129a98b4a5a151072c6b
Larissa Nagai <larissayukanagai@gmail.com>
2024-02-07 03:36:20 +0000

    modelo final

--- tests/teste_fct_vendas_2011.sql
+++ tests/teste_fct_vendas_2011.sql
@@ -1,11 +1,11 @@
 with
 
-    validacao2 as (
+    vendas_2011 as (
         select sum(faturamento_bruto) as soma_valor_bruto2
         from {{ ref('fct_vendas') }}
         where data_pedido between "2011-01-01" and "2011-12-31"
     )
 
 select *
-from validacao2
+from vendas_2011
 where soma_valor_bruto2 != 12646112.1607
